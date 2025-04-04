390534ce09be4ff5a88468f8859b801ca37df26b
yash meena <meenayash0910@gmail.com>
2025-04-04 23:52:15 +0530

    commit

---
+++ array/02/reverse_array.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"lenth of array\n";
+    cin>>n;
+    int a[n];
+for ( int i = 0; i < n; i++)
+{
+    cin>>a[n];
+}
+for(int i=0;i<n;i++)
+{
+ 
+    
+}
+}
